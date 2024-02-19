31f3242af80763f9d8cf4e5eec64c613c445f296
Pablo <pabloborregolunaiz@gmail.com>
2024-02-19 19:03:19 +0100

    first commit

---
+++ src/app/actual-map/actual-map.component.ts
@@ -0,0 +1,23 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {UbicacionActualService} from "../ubicacionActual.service";
+import {HousingService} from "../housing.service";
+
+
+@Component({
+  selector: 'app-actual-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './actual-map.component.html',
+  styleUrl: './actual-map.component.css'
+})
+export class ActualMapComponent implements OnInit {
+
+  route: ActivatedRoute = inject(ActivatedRoute);
+  ubicaionActual = inject(UbicacionActualService);
+  constructor() {};
+
+  ngOnInit(): void {
+    this.ubicaionActual.showYourLocation()
+  }
+}
